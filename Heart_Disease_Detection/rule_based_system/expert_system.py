0f4cc1f65a3d624feaae9b3a5b911f3fc9747ab5
ahd-osama <142544159+ahd-osama@users.noreply.github.com>
2025-03-22 15:45:18 +0200

    Update expert_system.py

--- Heart_Disease_Detection/rule_based_system/expert_system.py
+++ Heart_Disease_Detection/rule_based_system/expert_system.py
@@ -1,1 +1,30 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyOe0GR2awzp7yMTkm/xI1od"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":null,"metadata":{"id":"udWvoUmfiMAJ"},"outputs":[],"source":[]}]}
+from rules import HeartDiseaseExpert
+import pandas as pd
+from experta import *
+
+data = pd.read_csv("../data/cleaned_data.csv")
+
+engine = HeartDiseaseExpert()
+engine.reset()
+
+patient = {
+    "age": 58,
+    "sex": 0,
+    "cp": 0,
+    "trestbps": 100,
+    "chol": 248,
+    "fbs": 0,
+    "restecg": 0,
+    "thalach": 122,
+    "exang": 0,
+    "oldpeak": 1,
+    "slope": 1,
+    "ca": 0,
+    "thal": 2,
+}
+
+for key, value in patient.items():
+    engine.declare(Fact(**{key: value}))
+
+engine.run()
+
