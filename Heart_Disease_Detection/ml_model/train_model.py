0912d08bc0d3785fa3d2abd2d81edf9c39254d15
ahd-osama <142544159+ahd-osama@users.noreply.github.com>
2025-03-22 15:37:20 +0200

    Update train_model.py

--- Heart_Disease_Detection/ml_model/train_model.py
+++ Heart_Disease_Detection/ml_model/train_model.py
@@ -1,1 +1,32 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[]},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":1,"metadata":{"id":"n-Itg58FiUKv","executionInfo":{"status":"ok","timestamp":1742427978276,"user_tz":-120,"elapsed":38,"user":{"displayName":"Youssef Efat Wassef","userId":"00445079099820868526"}}},"outputs":[],"source":["#helloooo"]}]}
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import GridSearchCV
+import joblib
+
+data = pd.read_csv("../data/cleaned_data.csv")
+
+actualdata = data.drop(columns=['target'])
+labels = data['target']
+
+actualdata_train, actualdata_test, labels_train, labels_test = train_test_split(actualdata, labels, test_size=0.2, random_state=42, stratify=labels)
+
+actualdata_test.to_csv("../data/test_data.csv", index=False)
+labels_test.to_csv("../data/test_labels.csv", index=False)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+param_grid = {
+    'max_depth': [3, 5, 10, None],
+    'min_samples_split': [2, 5, 10]
+}
+
+grid_search = GridSearchCV(clf, param_grid, cv=5, scoring='accuracy')
+grid_search.fit(actualdata_train, labels_train)
+
+best_model = grid_search.best_estimator_
+best_model.fit(actualdata_train, labels_train)
+
+joblib.dump(best_model, "../data/model.pkl")
+print("Model saved successfully!")
