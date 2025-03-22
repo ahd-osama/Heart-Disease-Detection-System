b308fe02816c289c0d197f186a9307a2414ba2da
ahd-osama <142544159+ahd-osama@users.noreply.github.com>
2025-03-22 15:35:05 +0200

    Update predict.py

--- Heart_Disease_Detection/ml_model/predict.py
+++ Heart_Disease_Detection/ml_model/predict.py
@@ -1,1 +1,32 @@
-{"nbformat":4,"nbformat_minor":0,"metadata":{"colab":{"provenance":[],"authorship_tag":"ABX9TyOe0GR2awzp7yMTkm/xI1od"},"kernelspec":{"name":"python3","display_name":"Python 3"},"language_info":{"name":"python"}},"cells":[{"cell_type":"code","execution_count":null,"metadata":{"id":"ecLhmfN1jFma"},"outputs":[],"source":[]}]}
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
+
+test = pd.read_csv("../data/test_data.csv")
+actual = pd.read_csv("../data/test_labels.csv")
+
+model = joblib.load("../data/model.pkl")
+
+pred = model.predict(test)
+
+pd.DataFrame(pred, columns=["Predicted"]).to_csv("/content/drive/My Drive/Heart_Disease_Detection/data/predictions.csv", index=False)
+
+accuracy = accuracy_score(actual, pred)
+precision = precision_score(actual, pred)
+recall = recall_score(actual, pred)
+f1 = f1_score(actual, pred)
+
+print("Predictions saved successfully!")
+print(f"Accuracy: {accuracy:.4f}")
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1 Score: {f1:.4f}")
+
+plt.figure(figsize=(60, 15),dpi=300)
+plot_tree(model, feature_names=test.columns, class_names=["No Heart Disease", "Heart Disease"], filled=True, rounded=True ,
+          fontsize=8)
+plt.title("Decision Tree Visualization")
+plt.savefig("/content/drive/My Drive/Heart_Disease_Detection/data/decision_tree.png", bbox_inches='tight')
+plt.show()
