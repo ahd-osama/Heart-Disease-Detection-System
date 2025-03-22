a724a74fc2c0edd9ea9d0f86960c6481e42793bf
ahd-osama <142544159+ahd-osama@users.noreply.github.com>
2025-03-22 16:54:18 +0200

    Update app.py

--- Heart_Disease_Detection/ui/app.py
+++ Heart_Disease_Detection/ui/app.py
@@ -10,7 +10,7 @@ import joblib
 import math
 import sys 
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
 from Heart_Disease_Detection.rule_based_system.rules import HeartDiseaseExpert
 
